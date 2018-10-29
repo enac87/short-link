fe794a12f5ae5f0985b1616e4ab7f9ce2f38a4e5
Emaga Alain <alaincesar@hotmail.fr>
2018-10-29 16:20:27 +0100

    Some components added

--- client/main.js
+++ client/main.js
@@ -1,22 +1,25 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import { Meteor } from 'meteor/meteor';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Switch, Route } from 'react-router';
+import { BrowserRouter, browserHistory } from 'react-router-dom';
 
-import './main.html';
+import Login from '../imports/ui/Login';
+import Signup from '../imports/ui/Signup';
+import Link from '../imports/ui/Link';
+import NotFound from '../imports/ui/NotFound';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
+const routes = (
+	<BrowserRouter history={browserHistory}>
+		<Switch>
+			<Route path="/login" component={Login} />
+			<Route path="/signup" component={Signup} />
+			<Route path="/links" component={Link} />
+			<Route path="*" component={NotFound} />
+		</Switch>
+	</BrowserRouter>
+);
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+Meteor.startup(() => {
+	ReactDOM.render(routes, document.getElementById('app'));
 });
